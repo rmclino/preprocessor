056f42e946a29c1d431628ed1044787f1237dd51
Ricardo Lino <ricardo@costalino.com>
2019-05-22 10:43:51 -0300

    Ajuste Topico setup

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='rmclino_preprocessor',
       author = 'Ricardo Lino',
       author_email = 'ricardo@costalino.com',
       keywords =['rescale','preprocess','normalize','fillNa'],
-      classifiers = ['Development Status :: 3 - Alpha','Intended Audience :: Developers','Topic :: easy use','License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3' ],
+      classifiers = ['Development Status :: 3 - Alpha','Intended Audience :: Developers','Topic :: Education','License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3' ],
+      download_url ='https://github.com/rmclino/preprocessor/archive/1.0.tar.gz',
       zip_safe=False)
