883d367e2418a00bb43e04fde0b6aad207b01d91
Ricardo Lino <ricardo@costalino.com>
2019-05-22 10:26:38 -0300

    Ajustes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='rmclino_Preprocess', 
       version='1.0',
       descritption='Preprocess methods: fill missing values, rescale and normilize',
-      packages=['rmclino_preprocess'],
+      packages=['rmclino_preprocessor'],
       author = 'Ricardo Lino',
       author_email = 'ricardo@costalino.com',
       zip_safe=False)
