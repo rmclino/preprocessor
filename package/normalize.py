94227748669eeb2ce156f2e03e62bc765a83d3a2
Ricardo Lino <ricardo@costalino.com>
2019-05-22 10:10:13 -0300

    Class Normilize

---
+++ package/normalize.py
@@ -0,0 +1,18 @@
+from preprocessor import Preprocessor
+
+class normalize(Preprocessor):
+   
+    """ Normiliza class for receive data and return normalized data
+    
+            No Attributes   
+    """
+
+    def normalize(self, tipo = 'sample'):
+        if len(self.data) > 1:
+            self.get_mean()
+            self.get_stdev(tipo)
+            self.data = [ (x - self.mean) / self.stdev for x in self.data]
+            return self.data
+        else:
+            print("Fill data points to Rescale")
+    
