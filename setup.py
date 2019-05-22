82ea5e99e3e1e1b794ea4d47f3b8f99e0d4e3f94
Ricardo Lino <ricardo@costalino.com>
2019-05-22 10:36:54 -0300

    Ajuste Setup

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
-setup(name='rmclino_Preprocess', 
+setup(name='rmclino_preprocessor', 
       version='1.0',
-      descritption='Preprocess methods: fill missing values, rescale and normilize',
       packages=['rmclino_preprocessor'],
+      descritption='Preprocess methods: fill missing values, rescale and normilize',
       author = 'Ricardo Lino',
       author_email = 'ricardo@costalino.com',
+      keywords =['rescale','preprocess','normalize','fillNa'],
+      classifiers = ['Development Status :: 3 - Alpha','Intended Audience :: Developers','Topic :: easy use','License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3' ],
       zip_safe=False)
